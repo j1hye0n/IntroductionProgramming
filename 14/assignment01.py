2bcdf8e8d4ba7a7e14fe28804aba2c21a22976d0
j1hye0n <j1hyeon@sookmyung.ac.kr>
2025-06-04 19:04:07 +0900

    try7

--- 14/assignment01.py
+++ 14/assignment01.py
@@ -20,37 +20,47 @@ def cal (num1,num2,op) :
     
 prefin = set()
 def main (num_list,num_dict) :
+    target = int(num_list[0])
     for i in range(1,len(num_list)) :
-        a_ = num_list[i]
-        #print(len(num_list[i]))     #debug
-        #num1 = int(a_[1]) if len(num_list[i])==2 else int(a_[1]+a_[2])     # fixed below
-        num1 = int(''.join(str(bbin) for bbin in a_[1:]))
+        #a_ = num_list[i]
+        ##print(len(num_list[i]))     #debug
+        ##num1 = int(a_[1]) if len(num_list[i])==2 else int(a_[1]+a_[2])     # fixed below
+        #num1 = int(''.join(str(bbin) for bbin in a_[1:]))
         #print(f'first part : {num_dict.get(a_)} > num1 : {num1}') #debug
+        a = num_list[i]
+        num1 = int(a[1:])
         for j in range(1,len(num_list)) :
             if j!=i :
-                b = [ ch for ch in num_list[j]]
-                b_ = num_list[j]
-                #num2 = int(b[1]) if len(num_list[j])==2 else int(b[1]+b[2])    #fixed below
-                num2 = int(''.join(str(bbinb) for bbinb in b_[1:]))
+                #b = [ ch for ch in num_list[j]]
+                #b_ = num_list[j]
+                ##num2 = int(b[1]) if len(num_list[j])==2 else int(b[1]+b[2])    #fixed below
+                #num2 = int(''.join(str(bbinb) for bbinb in b_[1:]))
                 #print(f'second part : {num_dict.get(b_)} > num2 : {num2}')     #debug
+                b = num_list[j]
+                num2 =int(b[1:])
             else :
                 continue
             for k in range(1,len(num_list)) :
                 if k!= j and k!=i :
-                    c = [chs for chs in num_list[k]]
-                    c_ = num_list[k]
-                    #num3 = int(c[1]) if len(num_list[k])==2 else int(c[1]+c[2])    # fixed below
-                    num3 = int(''.join(str(bbinc) for bbinc in c_[1:]))
+                    #c = [chs for chs in num_list[k]]
+                    #c_ = num_list[k]
+                    ##num3 = int(c[1]) if len(num_list[k])==2 else int(c[1]+c[2])    # fixed below
+                    #num3 = int(''.join(str(bbinc) for bbinc in c_[1:]))
                     #print(f'third part : {num_dict.get(c_)} > num3 : {num3}')     #debug
+                    c = num_list[k]
+                    num3 = int(c[1:])
                 else :
                     continue
 
-                step_result = oper(num1,num2,num3,b[0],c[0])
+                #step_result = oper(num1,num2,num3,b[0],c[0])
 
-                if (step_result == int(num_list[0])) :
-                    prefin.add(num_dict.get(a_)+num_dict.get(b_)+num_dict.get(c_))
-                    #print(f"{num1}{b[0]}{num2}{c[0]}{num3}")
-                    #print(f"{a_}{b_}{c_} = {step_result} \n >> {num_dict.get(a_)}{num_dict.get(b_)}{num_dict.get(c_)}")      #debug
+                #if (step_result == int(num_list[0])) :
+                    #prefin.add(num_dict.get(a)+num_dict.get(b_)+num_dict.get(c_))
+                    ##print(f"{num1}{b[0]}{num2}{c[0]}{num3}")
+                    ##print(f"{a_}{b_}{c_} = {step_result} \n >> {num_dict.get(a_)}{num_dict.get(b_)}{num_dict.get(c_)}")      #debug
+                step_result = oper(num1,num2,num3,b[0],c[0])
+                if step_result==target:
+                    prefin.add(num_dict.get(a)+num_dict.get(b)+num_dict.get(c))
 
     return prefin
 
